77c6ff981d880f41967dd1180a07ff3940e1d768
Martin Betz <martinmichaelbetz@gmail.com>
2020-05-03 13:55:19 +0200

    Fix: Disable exception handling

--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -9,6 +9,7 @@ class WelcomeTest extends TestCase
     /** @test */
     public function can_not_access_welcome_page_without_pin()
     {
+        $this->withoutExceptionHandling();
         $response = $this->get(route('welcome'));
         $response->assertStatus(302);
         $response->assertRedirect(route('pin.create'));
